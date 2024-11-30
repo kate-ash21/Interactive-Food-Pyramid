ee3859c2b1b9d68a31c3291fe23fec2517a71d26
Katelan Ashley <katelan.macaspac.2023@mumail.ie>
2024-11-30 13:55:29 +0000

    Add files via upload

---
+++ assignment-01.js
@@ -0,0 +1,137 @@
+let redCount = 2;
+
+    document.getElementById("red-subtract").onclick = function()
+    {
+        redCount-=1;
+        document.getElementById("red-number").innerHTML = redCount;
+
+        if(redCount < 0)
+        {
+            redCount=0;
+            document.getElementById("red-number").innerHTML = redCount;
+        }
+
+    }
+
+    document.getElementById("red-add").onclick = function()
+    {
+        redCount+=1;
+        document.getElementById("red-number").innerHTML = redCount;
+        
+    }
+//------------------------------------------------------------------
+
+let orangeCount = 4;
+
+    document.getElementById("orange-subtract").onclick = function()
+    {
+        orangeCount-=1;
+        document.getElementById("orange-number").innerHTML = orangeCount;
+
+        if(orangeCount < 0)
+        {
+            orangeCount=0;
+            document.getElementById("orange-number").innerHTML = orangeCount;
+        }
+
+    }
+
+    document.getElementById("orange-add").onclick = function()
+    {
+        orangeCount+=1;
+        document.getElementById("orange-number").innerHTML = orangeCount;
+        
+    }
+//------------------------------------------------------------------
+    
+let yellowCount = 3;
+
+    document.getElementById("yellow-subtract").onclick = function()
+    {
+        yellowCount-=1;
+        document.getElementById("yellow-number").innerHTML = yellowCount;
+
+        if(yellowCount < 0)
+        {
+            yellowCount=0;
+            document.getElementById("yellow-number").innerHTML = yellowCount;
+        }
+
+    }
+
+    document.getElementById("yellow-add").onclick = function()
+    {
+        yellowCount+=1;
+        document.getElementById("yellow-number").innerHTML = yellowCount;
+        
+    }
+//------------------------------------------------------------------
+
+let blueCount = 3;
+
+    document.getElementById("blue-subtract").onclick = function()
+    {
+        blueCount-=1;
+        document.getElementById("blue-number").innerHTML = blueCount;
+
+        if(blueCount < 0)
+        {
+            blueCount=0;
+            document.getElementById("blue-number").innerHTML = blueCount;
+        }
+
+    }
+
+    document.getElementById("blue-add").onclick = function()
+    {
+        blueCount+=1;
+        document.getElementById("blue-number").innerHTML = blueCount;
+        
+    }
+//------------------------------------------------------------------
+
+let brownCount = 5;
+
+    document.getElementById("brown-subtract").onclick = function()
+    {
+        brownCount-=1;
+        document.getElementById("brown-number").innerHTML = brownCount;
+
+        if(brownCount < 0)
+        {
+            brownCount=0;
+            document.getElementById("brown-number").innerHTML = brownCount;
+        }
+
+    }
+
+    document.getElementById("brown-add").onclick = function()
+    {
+        brownCount+=1;
+        document.getElementById("brown-number").innerHTML = brownCount;
+        
+    }
+//------------------------------------------------------------------
+
+let greenCount = 6;
+
+    document.getElementById("green-subtract").onclick = function()
+    {
+        greenCount-=1;
+        document.getElementById("green-number").innerHTML = greenCount;
+
+        if(greenCount < 0)
+        {
+            greenCount=0;
+            document.getElementById("green-number").innerHTML = greenCount;
+        }
+
+    }
+
+    document.getElementById("green-add").onclick = function()
+    {
+        greenCount+=1;
+        document.getElementById("green-number").innerHTML = greenCount;
+        
+    }
+//-------------------------------JavaScriipt for button count---------------------------------------
